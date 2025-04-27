7b1603a4dc4a1e0d0229136360add76a1d48fc06
LeandroOliveiraDev <lleandro.silva@outlook.com>
2025-04-27 18:54:37 -0300

    primeiro commit inicio do projeto

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { Post } from "./post"
+
+
+
+export function App() {
+
+
+  return (
+    <div>
+      <Post author="Leandro" content="Lorem ipsum dolor sit amet" />
+      <Post author="Fulano" content="novo post aqui mano" />
+
+
+    </div>
+  )
+}
+
+
---
+++ src/Post.jsx
@@ -0,0 +1,8 @@
+export function Post(props) {
+    return (
+        <div>
+            <strong>{props.content}</strong>
+            <p>{props.content}</p>
+        </div>
+    )
+}
